c5ea15076e7a452726c5509853e41bb9eecab545
Ivan Kolobaev <ikolobai@yandex.ru>
2018-10-31 01:35:08 +0300

    Убран выпадающий список меню

--- scripts.js
+++ scripts.js
@@ -233,8 +233,6 @@ arrowLeftScens.addEventListener('click', function () {
 });
 
 const selectButton = document.querySelector('.filter__select-button');
-const selectButtonText = document.querySelector('.filter__select-button .button__text');
-const selectOptions = document.querySelectorAll('.filter__select-item');
 const popup = document.querySelector('.filter__select-popup');
 
 selectButton.addEventListener('click', function() {
@@ -245,15 +243,4 @@ let widths = '';
 window.addEventListener('scroll', function() {
     widths += document.querySelectorAll('body')[0].offsetWidth;
     document.querySelector('.stats').innerHTML = widths;
-});
-
-selectOptions.forEach(o => {
-  o.addEventListener('click', function(e) {
-    document.querySelector('#' + e.target.dataset.group).checked = true;
-
-    selectOptions.forEach(opt => opt.classList.toggle('filter__select-item_checked', false));
-    e.target.classList.toggle('filter__select-item_checked', true);
-    popup.classList.toggle('filter__select-popup_open', false);
-    selectButtonText.innerText = e.target.innerText;
-  })
 });
